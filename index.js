ce3675e6b041ff201daf9b867e79e92a7869e9e1
Olga <grunichevaolha@yandex.ru>
2022-12-11 12:01:20 +0300

    button

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 $('.js-navigation-page').on('click', function(ev) {
-    var dataAttr = ev.currentTarget.getAttribute('data-page')
+    $('.js-navigation-page').each(function(index, elm) {
+        var $el = $(elm);
+        $el.removeClass('navigation_active');
+    });
+    $(ev.target).addClass('navigation_active');
+    var dataAttr = ev.currentTarget.getAttribute('data-page');
     var pages = $('.page');
     pages.removeClass('page-show');
     pages.each(function(index, elm) {
         var $el = $(elm);
-        var dataAtt = $el.attr('data-page')
+        var dataAtt = $el.attr('data-page');
         if (dataAtt === dataAttr) {
-            $el.addClass('page-show')
+            $el.addClass('page-show');
         }
-    })
+    });
 });
